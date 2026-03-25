490245df31b07c66b2383fe7108e58505721a52c
GuillianeDieppois <Guilliane.dieppois@outlook.fr>
2026-03-25 12:37:43 +0100

    Ajout ordre décroissant

--- Eleve.py
+++ Eleve.py
@@ -15,13 +15,16 @@ class Class:
     def add_student(self, student):
         self.students.append(student)
 
+    def rank_matter_1(self):
+        return sorted(self.students, key=lambda s: s.Archi_appli, reverse=True)
+
 if __name__ == '__main__':
     school_class = Class()
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
 
-    print(school_class.students)
+    print(school_class.rank_matter_1())
 
 
 
