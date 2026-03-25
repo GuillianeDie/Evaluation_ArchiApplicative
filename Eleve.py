f83f70414a7ce097b53b47b6bfa92581b29cbe08
GuillianeDieppois <Guilliane.dieppois@outlook.fr>
2026-03-25 12:35:23 +0100

    Ajout du code en fin de script

--- Eleve.py
+++ Eleve.py
@@ -8,9 +8,20 @@ class Student:
     def __repr__(self):
         return f"Student({self.nom}, {self.Archi_appli}, {self.Monitoring}, {self.Integr_Continue})"
 
-class SchoolClass:
+class Class:
     def __init__(self):
         self.students = []
 
     def add_student(self, student):
         self.students.append(student)
+
+if __name__ == '__main__':
+    school_class = Class()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
+
+    print(school_class.students)
+
+
+
